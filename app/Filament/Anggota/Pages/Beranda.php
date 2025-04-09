1a4870580a93d3840a7c1d07d3722b558fa3acdd
yudas19 <yudastiawan90@gmail.com>
2025-04-09 19:21:01 +0700

    Menambahkan value str, pendidikan, pekerjaan di page beranda/anggota

--- app/Filament/Anggota/Pages/Beranda.php
+++ app/Filament/Anggota/Pages/Beranda.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace App\Filament\Anggota\Pages;
-
+use App\models\pendidikan;
+use App\Models\str;
+use App\Models\pekerjaan;
+use App\Models\anggota;
 use Filament\Pages\Page;
 use Illuminate\Support\Facades\Auth;
 
@@ -20,10 +23,20 @@ class Beranda extends Page
     // Page Data
     
     public $user;
+    public $str;
+    public $pendidikan;
+    public $pekerjaan;
+    public $anggota;
 
     public function mount()
     {
         $this->user = Auth::user();
-    }
+
+        // Mengambil data NO STR, pendidikan > Alumnus, pekerjaan > Mulai bekerja,user yang sedang login berdasarkan NIK
+        $this->str = Str::where('nik', $this->user->nik)->first();
+        $this->pendidikan = Pendidikan::where('nik', $this->user->nik)->first();
+        $this->pekerjaan = Pekerjaan::where('nik', $this->user->nik)->first();
+        $this->anggota = Anggota::where('nik' , $this->user->nik)->first();
+        }
 
 }
